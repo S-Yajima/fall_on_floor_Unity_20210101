8093dc10feefab4b85394e1aa01a0b79fcdcbdf8
S-Yajima <shige048jp@gmail.com>
2021-01-01 13:25:05 +0900

    キューブ回転＆パーティクル追加
    キューブ回転＆パーティクル追加

---
+++ Assets/Scripts/Director.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/*
+ * ディレクタースクリプト
+ * 時間の経過とともにキューブとスフィアを順次落下させる
+ */
+public class Director : MonoBehaviour
+{
+    private GameObject cube;
+    private GameObject Sphere_blue;
+    private GameObject Sphere_red;
+    private GameObject Sphere_green;
+    private GameObject Sphere_yellow;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // キューブとスフィアのGameObjectへの参照を取得する
+        this.cube = GameObject.Find("Cube");
+        this.Sphere_blue = GameObject.Find("Sphere_blue");
+        this.Sphere_red = GameObject.Find("Sphere_red");
+        this.Sphere_green = GameObject.Find("Sphere_green");
+        this.Sphere_yellow = GameObject.Find("Sphere_yellow");
+
+        // 時間経過により呼び出すメソッドをInvoke()により設定する
+        // 注意: Invoke()へ引数は渡せない
+        Invoke("fall_cube", 3.0f);
+        Invoke("fall_Sphere_blue", 10.0f);
+        Invoke("fall_Sphere_red", 20.0f);
+        Invoke("fall_Sphere_green", 30.0f);
+        Invoke("fall_Sphere_yellow", 40.0f);
+    }
+
+    // キューブを落下させる
+    void fall_cube()
+    {
+        this.cube.GetComponent<Rigidbody>().useGravity = true;
+    }
+
+    // スフィア(青)を落下させる
+    void fall_Sphere_blue()
+    {
+        this.Sphere_blue.GetComponent<Rigidbody>().useGravity = true;
+    }
+
+    // スフィア(赤)を落下させる
+    void fall_Sphere_red()
+    {
+        this.Sphere_red.GetComponent<Rigidbody>().useGravity = true;
+    }
+
+    void fall_Sphere_green()
+    {
+        this.Sphere_green.GetComponent<Rigidbody>().useGravity = true;
+    }
+
+    void fall_Sphere_yellow()
+    {
+        this.Sphere_yellow.GetComponent<Rigidbody>().useGravity = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PlaneController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * 床のコントローラ
+ * Sceneの始まりでrotate_max_countの数値回数だけ回転する
+ */
+public class PlaneController : MonoBehaviour
+{
+    // x軸回転角度 初期値 90°
+    private float angle_x = 90;
+    // 回転最大回数と回転した回数
+    private int rotate_max_count = 2;
+    private int rotate_count = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 設定した回数回っていたらメソッドを抜ける
+        if (this.rotate_count >= this.rotate_max_count) return;
+
+        // 20度ずつ回転するように設定する
+        this.angle_x += 20.0f;
+
+        // 角度が360を超えていたら0に戻す
+        if (this.angle_x >= 360.0f){
+            this.angle_x = 0.0f;
+            this.rotate_count++;
+        }
+
+        // 算出した床の回転を実行する
+        Vector3 angles = transform.localEulerAngles;
+        angles.x = angle_x;
+        angles.y = 0.0f;    // 0.0f以外の数値が取得されるケースがあるため明示的に0.0fを代入する
+        angles.z = 0.0f;
+        transform.localEulerAngles = angles;
+    }
+}
